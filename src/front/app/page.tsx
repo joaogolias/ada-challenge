fbc891a6e33318e8b50de7db57da3166d5d315af
João Victor Golias <joao.golias@orbia.ag>
2023-12-10 11:36:17 -0300

    implemented header component

--- src/front/app/page.tsx
+++ src/front/app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import { Header } from '@/components/Header';
 import { CardContainer } from '../components/CardContainer';
 
 export default function Home() {
   return (
-    <div className="flex">
+    <div>
+      <Header />
       <CardContainer />
     </div>
   );
---
+++ src/front/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+export const Header: React.FC = () => {
+  return (
+    <div className="flex justify-center border-[2px] w-full min-h-[48px]">
+      <Typography className="self-center"> ADA CHALLENGE - KIBANA </Typography>
+    </div>
+  );
+};
